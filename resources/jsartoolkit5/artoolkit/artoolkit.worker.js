7f4c8fe5ba81df82c37eafbb7a263087b7d36beb
kalwalt <github@kalwaltart.it>
2019-12-14 17:10:04 +0100

    cubist-dragon example refactoring

---
+++ resources/jsartoolkit5/artoolkit/artoolkit.worker.js
@@ -0,0 +1,57 @@
+importScripts('../standard/artoolkitNft.min.js');
+
+self.onmessage = e => {
+    let msg = e.data;
+    switch (msg.type) {
+        case "load": {
+            load(msg);
+            return;
+        }
+        case "process": {
+            next = msg.imagedata;
+            process();
+            return;
+        }
+    }
+};
+
+let next = null;
+
+let ar = null;
+let markerResult = null;
+
+function load(msg) {
+    let param = new ARCameraParam(msg.camera_para);
+    param.onload = function () {
+        ar = new ARController(msg.pw, msg.ph, param);
+        let cameraMatrix = ar.getCameraMatrix();
+
+        ar.addEventListener('getNFTMarker', function (ev) {
+            markerResult = {type: "found", matrixGL_RH: JSON.stringify(ev.data.matrixGL_RH), proj: JSON.stringify(cameraMatrix)};
+        });
+
+        ar.loadNFTMarker(msg.marker, function (markerId) {
+            ar.trackNFTMarkerId(markerId, 2);
+            console.log("loadNFTMarker -> ", markerId);
+        });
+
+        postMessage({type: "loaded", proj: JSON.stringify(cameraMatrix)});
+    };
+}
+
+function process() {
+
+    markerResult = null;
+
+    if (ar) {
+        ar.process(next);
+    }
+
+    if (markerResult) {
+        postMessage(markerResult);
+    } else {
+        postMessage({type: "not found"});
+    }
+
+    next = null;
+}
