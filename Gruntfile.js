faaa8af2e7d278db1204a3694163fb322731e363
Walter Perdan <github@kalwaltart.it>
2020-04-06 16:49:57 +0200

    minified version of nftLoader

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/*global module,require */
+module.exports = function(grunt) {
+  "use strict";
+
+  var pkg = grunt.file.readJSON("package.json");
+
+  grunt.initConfig({
+    pkg: pkg,
+
+    uglify: {
+      options: {},
+      dist: {
+        src: "resources/js/nftLoader/nftLoader.js",
+        dest: "resources/build/nftLoader/nftLoader.min.js"
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+};
